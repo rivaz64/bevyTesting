1a4fb45b5760c06b01456c0ffdb46a8855104fe5
rivaz64 <59717197+rivaz64@users.noreply.github.com>
2023-11-10 10:45:59 -0600

    collicion and overlap separated

--- src/Collicions.rs
+++ src/Collicions.rs
@@ -1,16 +1,38 @@
 use bevy::prelude::*;
-fn collicionPointCircle(point: Vec3, circle:  Transform)->bool
-{
-    let size = circle.scale.x*0.5;
-    (point-circle.translation).length_squared()<size*size
+
+struct Collicion{
+    point : Vec3,
+    normal : Vec3,
+    depth : f32
 }
 
-fn collicionCircleCircle(circle1:  Transform, circle2:  Transform)->bool
+fn overlap_point_sphere(point: Vec3, sphere:  Transform)->bool
 {
-    let size = (circle1.scale.x+circle2.scale.x)*0.5;
-    (circle2.translation-circle1.translation).length_squared()<size*size
+    let size = sphere.scale.x*0.5;
+    (point-sphere.translation).length_squared()<size*size
 }
 
-fn collicionPlaneCirlce(circle:  Transform, plane:  Transform)->bool{
-    (circle.translation.dot(plane.up())-plane.translation.dot(plane.up())).abs()<circle.scale.x*0.5
+fn overlap_sphere_sphere(sphere1:  Transform, sphere2:  Transform)->bool
+{
+    let size = (sphere1.scale.x+sphere2.scale.x)*0.5;
+    (sphere2.translation-sphere1.translation).length_squared()<size*size
+}
+
+fn collicion_sphere_sphere(sphere1:  Transform, sphere2:  Transform)->Option<(Collicion,Collicion)>
+{
+    let size = (sphere1.scale.x+sphere2.scale.x)*0.5;
+    let distance_sqrd = (sphere2.translation-sphere1.translation).length_squared();
+    if !(distance_sqrd<size*size){
+        None
+    }
+    else{
+        let normal = (sphere2.translation-sphere1.translation).normalize();
+        let depth = size-distance_sqrd.sqrt();
+        Some((Collicion{normal: normal,point:sphere1.translation+normal*sphere1.scale.x,depth:depth},
+            Collicion{normal: -normal,point:sphere2.translation-normal*sphere2.scale.x,depth:depth}))
+    }
+}
+
+fn overlap_sphere_plane(sphere:  Transform, plane:  Transform)->bool{
+    (sphere.translation.dot(plane.up())-plane.translation.dot(plane.up())).abs()<sphere.scale.x*0.5
 }
